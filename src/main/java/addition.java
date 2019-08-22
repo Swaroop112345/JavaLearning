77995ef02b107ff6bbb3a93877b345ce6f383259
Swaroop <swaroop0140@gmail.com>
2019-08-22 22:08:58 +0300

    1st commit

---
+++ src/main/java/addition.java
@@ -0,0 +1,17 @@
+public class addition {
+
+
+    public static int add(int number1,int number2)
+    {
+        System.out.println("1st Number is " + number1);
+        System.out.println("1st Number is " + number2);
+
+        int add=number1+number2;
+
+        System.out.println("Addition of two numbers is " + add);
+        return add;
+    }
+
+}
+
+
